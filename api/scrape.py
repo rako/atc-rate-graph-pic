090a0f4555f07cd3d71bd2df8e03e496a764478a
rako <=>
2024-10-03 22:31:53 +0900

    全体的な修正

--- api/scrape.py
+++ api/scrape.py
@@ -7,20 +7,18 @@ from flask import Flask, jsonify, request, render_template
 from datetime import datetime
 import os
 
-#Flaskのセットアップ
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#/api/scrapeエンドポイントのGETリクエストを処理
 @app.route('/api/scrape', methods=['POST'])
-def scrape(): #スクレイピング処理
-    username = request.form['username'] #usernameを取得
-    if not username: #usernameが指定されていない場合
+def scrape():
+    username = request.form['username']
+    if not username:
         return jsonify({'error': 'Username is required'}), 400
-    
+
     url = f'https://atcoder.jp/users/{username}'
 
     try:
@@ -35,15 +33,15 @@ def scrape(): #スクレイピング処理
         # ページが完全に読み込まれるまで待機 (最大10秒), 1秒単位
         driver.implicitly_wait(10)
 
-        # canvas要素を取得
-        canvas = driver.find_element_by_tag_name('ratingGraph')
+        # canvas要素をIDで取得
+        canvas = driver.find_element_by_id('ratingGraph')
 
         # canvas要素のスクリーンショットを撮る
         canvas_screenshot = canvas.screenshot_as_png
 
         # スクリーンショットをPillowで画像として保存
         image = Image.open(io.BytesIO(canvas_screenshot))
-        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S') # 現在時刻を取得
+        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')  # 現在時刻を取得
         filename = f'rating_graph_image_{username}_{timestamp}.png'
         image_path = os.path.join(app.static_folder, filename)
         image.save(image_path)
@@ -55,10 +53,6 @@ def scrape(): #スクレイピング処理
         image_url = f'/static/{filename}'
 
         return jsonify({'message': f'スクリーンショットが保存されました: {filename}', 'url': image_url})
-        
-        #スクレイピング結果URLをクリップボードにコピーする
-
-
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
